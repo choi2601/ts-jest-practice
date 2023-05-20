b39a46f050813cd6b098f445280ea0022aa0a09b
choi2601 <byeonghyeonchoe55@gmail.com>
2023-05-20 22:08:00 +0900

    triple A principles

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,18 @@
 import { toUpperCase } from "../app/Utils";
 
 describe("Unit test suite", () => {
-  test("should return uppercase", () => {
-    const result = toUpperCase("abc");
-    expect(result).toBe("ABC");
+  it("should return uppercase of valid string", () => {
+    // arrage:
+    const sut = toUpperCase;
+    const expected = "ABC";
+
+    // act:
+    const actual = sut("abc");
+
+    // assert:
+    expect(actual).toBe(expected);
+
+    // const result = toUpperCase("abc");
+    // expect(result).toBe("ABC");
   });
 });
