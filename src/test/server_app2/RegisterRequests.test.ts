ad63ab0cca20b92463cde862d050f31c5ccf3f12
choi2601 <byeonghyeonchoe55@gmail.com>
2023-06-20 22:17:47 +0900

    Low mocks set up test

---
+++ src/test/server_app2/RegisterRequests.test.ts
@@ -0,0 +1,52 @@
+import { Server } from "../../app/server_app/server/Server";
+import { DataBase } from "../../app/server_app/data/DataBase";
+import { RequestTestWrapper } from "./test_utils/RequestTestWrapper";
+import { ResponseTestWrapper } from "./test_utils/ResponseTestWrapper";
+import {
+  HTTP_CODES,
+  HTTP_METHODS,
+} from "../../app/server_app/model/ServerModel";
+
+jest.mock("../../app/server_app/data/DataBase");
+
+const requestWrapper = new RequestTestWrapper();
+const responseWrapper = new ResponseTestWrapper();
+
+const fakeServer = {
+  listen: () => {},
+  close: () => {},
+};
+
+jest.mock("http", () => ({
+  createServer: (cb: Function) => {
+    cb(requestWrapper, responseWrapper);
+    return fakeServer;
+  },
+}));
+
+describe("Register requests test suite", () => {
+  afterEach(() => {
+    requestWrapper.clearFileds();
+    responseWrapper.clearFields();
+  });
+
+  it("should work", async () => {
+    requestWrapper.method = HTTP_METHODS.POST;
+    requestWrapper.body = {
+      userName: "someUserName",
+      password: "somePassword",
+    };
+    requestWrapper.url = "localhost:8080:register";
+
+    await new Server().startServer();
+
+    await new Promise(process.nextTick); // this solves timing issues
+
+    expect(responseWrapper.statusCode).toBe(HTTP_CODES.CREATED);
+    expect(responseWrapper.body).toEqual(
+      expect.objectContaining({
+        userId: expect.any(String),
+      })
+    );
+  });
+});
